3956909d52082c7e0f2e015b71b432d9087e7b67
mrstnj <>
2024-10-23 22:58:30 +0900

    add: デザイン調整

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,22 @@ import 'package:flutter/material.dart';
 import 'api_key.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
+}
+
+class Message {
+  const Message(
+    this.message,
+    this.sendTime, {
+    required this.fromChatGpt,
+  });
+  final String message;
+  final bool fromChatGpt;
+  final DateTime sendTime;
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -40,8 +51,37 @@ class _MyHomePageState extends State<MyHomePage> {
   final _textEditingController = TextEditingController();
   var _answer = '';
 
+  final _messages = <Message>[
+    Message("お疲れ様！今日はどうだった？", DateTime(2024, 10, 23, 0, 0), fromChatGpt: false),
+    Message("お疲れ様！今日はちょっとバタバタしてたけど、無事終わったよ。", DateTime(2024, 10, 23, 1, 0),
+        fromChatGpt: true),
+    Message("おー、よかったね！仕事終わりになんか予定あるの？", DateTime(2024, 10, 23, 2, 0),
+        fromChatGpt: false),
+    Message("特にないかなー。家でNetflixでも見ようかなって。", DateTime(2024, 10, 23, 3, 0),
+        fromChatGpt: true),
+    Message("Netflixいいね！最近面白いのあった？", DateTime(2024, 10, 23, 4, 0),
+        fromChatGpt: false),
+    Message("「ワンピース」の実写版見たけど、思ったより良かったよ！", DateTime(2024, 10, 23, 5, 0),
+        fromChatGpt: true),
+    Message("マジ？原作ファンだからちょっと不安だったんだけど、見る価値ある？", DateTime(2024, 10, 23, 6, 0),
+        fromChatGpt: false),
+    Message("あると思う！設定も忠実だし、キャストも結構頑張ってる感じ。", DateTime(2024, 10, 23, 7, 0),
+        fromChatGpt: true),
+    Message("じゃあ、週末に見てみようかな。おすすめありがとう！", DateTime(2024, 10, 23, 8, 0),
+        fromChatGpt: false),
+    Message("ぜひぜひ！感想も聞かせてねー。", DateTime(2024, 10, 23, 9, 0),
+        fromChatGpt: true),
+  ];
+
+  // static Color colorBackground = Color.fromARGB(0xFF, 0x90, 0xac, 0xd7);
+  // static Color colorMyMessage = Color.fromARGB(0xFF, 0x8a, 0xe1, 0x7e);
+  // static Color colorOthersMessage = Color.fromARGB(0xFF, 0xff, 0xff, 0xff);
+  // static Color colorTime = Color.fromARGB(0xFF, 0x72, 0x88, 0xa8);
+
   @override
   Widget build(BuildContext context) {
+    final deviceWidth = MediaQuery.of(context).size.width;
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
@@ -51,15 +91,34 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Row(
-              children: [
-                Expanded(child: TextField(controller: _textEditingController )),
-                IconButton(onPressed: () async {
-                  final answer = await _sendMessage(_textEditingController.text);
-                  setState(() {
-                    _answer = answer;
-                  });
-                }, icon: Icon(Icons.send)),
+            Expanded(
+              child: ListView.builder(
+                itemCount: _messages.length,
+                itemBuilder: (context, index) {
+                  return Row(
+                    children: [
+                      CircleAvatar(radius: 16, child: Icon(Icons.add)),
+                      ConstrainedBox(
+                        constraints:
+                          BoxConstraints(maxWidth: deviceWidth * 0.7),
+                        child: Text(_messages[index].message)
+                      ),
+                      Text('午前12:00')
+                    ],
+                  );
+                })
+              ),
+            Row(children: [
+              Expanded(child: TextField(controller: _textEditingController)),
+              IconButton(
+                  onPressed: () async {
+                    final answer =
+                        await _sendMessage(_textEditingController.text);
+                    setState(() {
+                      _answer = answer;
+                    });
+                  },
+                  icon: Icon(Icons.send)),
             ]),
             Text(_answer),
           ],
@@ -69,8 +128,9 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   Future<String> _sendMessage(String message) async {
-    final request = CompleteText(prompt: message, model: Gpt3TurboInstruct(), maxTokens: 200);
-  
+    final request = CompleteText(
+        prompt: message, model: Gpt3TurboInstruct(), maxTokens: 200);
+
     final response = await openAI.onCompletion(request: request);
     return response!.choices.first.text;
   }
